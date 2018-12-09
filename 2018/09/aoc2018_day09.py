e39b9381611f1d8ad17270ca87aed8c16fb050c5
Petr Prazak <prazakp@gmail.com>
2018-12-09 17:00:35 +0100

    2018 day 9 solution (yes, i reimplemented deque)

--- 2018/09/aoc2018_day09.py
+++ 2018/09/aoc2018_day09.py
@@ -2,57 +2,79 @@
 
 from __future__ import print_function
 from collections import defaultdict
-from time import  time
 
 INPUT = "aoc2018_day09.txt"
 
 
-def play(players, last_marble):
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class Game:
+    def __init__(self):
+        node = Node(0)
+        node.next = node
+        node.prev = node
+        self.cur_node = node
+
+    def add_marble(self, marble):
+        new_node = Node(marble)
+        after = self.cur_node.next
+        new_node.next = after.next
+        new_node.prev = after
+        after.next.prev = new_node
+        after.next = new_node
+        self.cur_node = new_node
+
+    def remove_marble(self):
+        removed = self.cur_node
+        for _ in range(7):
+            removed = removed.prev
+        removed.prev.next = removed.next
+        self.cur_node = removed.next
+        return removed.data
+
+    def print_list(self):
+        next_node = self.cur_node.next
+        print("[", self.cur_node.data, end="")
+        while next_node != self.cur_node:
+            print(",", next_node.data, end="")
+            next_node = next_node.next
+        print(" ]")
+
+
+def play2(players, last_marble):
     print(players, last_marble)
-    current = 0
-    desk = list()
-    desk.append(0)
-    cur_len = 1
+    desk = Game()
     player = 0
-    score = defaultdict(list)
-    last_time = time()
+    score = defaultdict(int)
     for marble in range(1, last_marble+1):
-        # cur_len = len(desk)
-        if marble % 100000 == 0:
-            next_time = time()
-            print(next_time - last_time, marble)
-            last_time = next_time
-
         if not marble % 23:
-            score[player].append(marble)
-            keep = (current + cur_len - 6) % cur_len
-            score[player].append(desk[keep])
-            del desk[keep]
-            current = keep - 1
-            cur_len -= 1
+            removed_marble = desk.remove_marble()
+            score[player] += marble + removed_marble
         else:
-            current = (current + 2) % cur_len
-            desk.insert(current+1, marble)
-            cur_len += 1
-
+            desk.add_marble(marble)
         player = (player + 1) % players
 
-    score_count = [(sum(score[p]), p) for p in score]
-    # print(score_count)
-    return max(score_count)
+    return max(score.values())
+
 
 # test data
-# 0 players; last marble is worth 1618 points: high score is 8317
+# 10 players; last marble is worth 1618 points: high score is 8317
 # 13 players; last marble is worth 7999 points: high score is 146373
 # 17 players; last marble is worth 1104 points: high score is 2764
 # 21 players; last marble is worth 6111 points: high score is 54718
 # 30 players; last marble is worth 5807 points: high score is 37305
 
+
 with open(INPUT) as f:
     data = f.readline().rstrip().split()
-    # print(play(9, 25))
-    # print(play(10, 1618))
-    # print(play(13,7999))
-    # print(play(30,5807))
-    print(play(int(data[0]), int(data[6])))
-    print(play(int(data[0]), int(data[6]) * 100))
+    # print(play2(9, 25))
+    assert(play2(10, 1618) == 8317)
+    assert(play2(13,7999) == 146373)
+    assert(play2(30,5807) == 37305)
+    print(play2(int(data[0]), int(data[6])))
+    print(play2(int(data[0]), int(data[6]) * 100))
