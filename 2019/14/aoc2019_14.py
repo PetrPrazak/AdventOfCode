18f889c2085eb89a03e4837bc2867a7cb037871d
Petr Prazak <prazakp@gmail.com>
2019-12-14 22:42:24 +0100

    little polishing

--- 2019/14/aoc2019_14.py
+++ 2019/14/aoc2019_14.py
@@ -9,23 +9,30 @@ from aoc import *
 INPUT = "aoc2019_14_input.txt"
 TEST = "test.txt"
 
+ORE = "ORE"
+FUEL = "FUEL"
+
+
+def parse_parts(left):
+    out = dict()
+    for single in left.split(', '):
+        x, y = single.split()
+        out[y] = int(x)
+    return out
+
 
 def parse_data(data):
     reactions = dict()
     for line in data:
         left, right = line.split(" => ")
         quantity, material = right.split()
-        out = dict()
-        for single in left.split(', '):
-            x, y = single.split()
-            out[y] = int(x)
-        reactions[material] = (int(quantity), out)
+        reactions[material] = (int(quantity), parse_parts(left))
     return reactions
 
 
 def factory(store, reactions, material, quantity):
     made, leftover = store.setdefault(material, (0, 0))
-    if material == "ORE":
+    if material == ORE:
         if leftover < quantity:
             raise StopIteration
         store[material] = made + quantity, leftover - quantity
@@ -38,38 +45,41 @@ def factory(store, reactions, material, quantity):
         factory(store, reactions, component, multiple * weight)
 
 
+cargo_ore = 1000000000000
+
+
+def run_factory(reactions, fuel_amount):
+    store = dict()
+    store[ORE] = (0, cargo_ore)
+    factory(store, reactions, FUEL, fuel_amount)
+    return store[ORE]
+
+
 @timeit
 def process(data):
     # part 1
     reactions = parse_data(data)
-    store = dict()
-    cargo_ore = 1000000000000
-    store["ORE"] = (0, cargo_ore)
-    factory(store, reactions, "FUEL", 1)
-    ore_per_fuel = store["ORE"][0]
+    ore_per_fuel, _ = run_factory(reactions, 1)
     print(ore_per_fuel)  # 1037742
 
     # part 2
     # find iteratively how much fuel can we produce
     # so the ore leftover is less then ore amount required for single fuel unit
-    increment = 1
+    # should have used binary search but this works as well...
+    increment = 0.1
     coef = 1
     startfuel = cargo_ore / ore_per_fuel
     fuelcount = None
     while True:
-        store = dict()
-        store["ORE"] = (0, cargo_ore)
         coef += increment
         fuelcount = int(startfuel * coef)
         try:
-            factory(store, reactions, "FUEL", fuelcount)
-            ore_left = store["ORE"][1]
+            _, ore_left = run_factory(reactions, fuelcount)
             if ore_left < ore_per_fuel:
                 break
         except StopIteration:
             coef -= increment
             increment /= 10
-
     print(fuelcount)  # 1572358
 
 
