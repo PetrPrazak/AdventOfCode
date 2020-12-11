ab043b793b0e7da3980e36d13a3735ade715c073
Petr Pražák <prazakp@gmail.com>
2020-12-11 18:21:27 +0100

    Advent of Code 2016 day 4
    add readme,
    refactor

--- 2016/Day04/day4.py
+++ 2016/Day04/day4.py
@@ -1,44 +1,53 @@
+# http://adventofcode.com/2016/day/4
 from __future__ import print_function
-
-"""
-http://adventofcode.com/2016/day/4
-"""
-
 import re
-from collections import Counter,defaultdict
+from collections import Counter, defaultdict
+
 
 regex = re.compile(r"([a-z-]+)-(\d+)\[([a-z]+)\]")
 
+
 def solve(lines):
-    sum  = 0
+    sum = 0
     for line in lines:
         r = regex.match(line)
-        name = r.group(1).replace('-','')
+        name = r.group(1).replace('-', '')
         letters = Counter(name)
         d = defaultdict(str)
-        for l in letters.keys():
+        for l in letters:
             d[letters[l]] += l
-        realname = "".join(["".join(sorted(d[k])) for k in sorted(d.keys(), reverse=True)])
+        realname = "".join(["".join(sorted(d[k]))
+                            for k in sorted(d.keys(), reverse=True)])
         checksum = r.group(3)
         if realname[:5] == checksum:
             num = int(r.group(2))
             sum += num
-    print(sum)
+    print("Part 1:", sum)
+
+
+def decrypt(char, room, modul):
+    return chr(ord('a') + ((ord(char) - ord('a') + room) % modul)) if char != ' ' else ' '
+
 
 def solve2(lines):
     modul = ord('z') - ord('a') + 1
     for line in lines:
         r = regex.match(line)
-        name = r.group(1).replace('-',' ')
+        name = r.group(1).replace('-', ' ')
         room = int(r.group(2))
-        realname = "".join([chr(ord('a') + ((ord(c) - ord('a') + room) % modul)) if c is not ' ' else ' ' for c in list(name)])
-        print(realname, room)
+        realname = "".join(decrypt(c, room, modul) for c in list(name))
+        if realname == 'northpole object storage':
+            print("Part 2:", room)
+            return
+
+
+def main(file):
+    with open(file) as f:
+        lines = f.readlines()
+        solve(lines)
+        solve2(lines)
+
 
 if __name__ == "__main__":
-    # with open("day4_input_test.txt") as f:
-    #     lines = f.readlines()
-    #     solve2(lines)
-    with open("day4_input.txt") as f:
-        lines = f.readlines()
-        solve2(lines)
- 
+    # main("day4_input_test.txt")
+    main("day4_input.txt")
