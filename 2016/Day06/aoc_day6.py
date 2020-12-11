6db6a15450a29887791675a23079e058bbe428ff
Petr Pražák <prazakp@gmail.com>
2020-12-11 19:00:19 +0100

    Advent of Code 2016 day 6
    add readme,
    refactor

--- 2016/Day06/aoc_day6.py
+++ 2016/Day06/aoc_day6.py
@@ -1,27 +1,24 @@
+# http://adventofcode.com/2016/day/6
 from __future__ import print_function
-from collections import Counter, defaultdict
+from collections import Counter
 
 
-def solve(lines, part=1):
-
-    cnt = []
+def solve(lines):
+    cnt = [Counter() for _ in range(len(lines[0]))]
     for line in lines:
-        line = line.strip()
-        if not cnt:
-            for i in range(len(line)):
-                cnt.append(Counter())
-
-        for pos,c in enumerate(list(line)):
+        for pos, c in enumerate(line):
             cnt[pos][c] += 1
 
-    print("".join([a.most_common(1)[0][0] for a in cnt]))
-    print("".join([a.most_common()[-1][0] for a in cnt])) #part 2
+    print("Part 1:", "".join([a.most_common(1)[0][0] for a in cnt]))
+    print("Part 2:", "".join([a.most_common()[-1][0] for a in cnt]))
 
 
-# INPUT = "aoc_day6_test.txt"
-INPUT = "aoc_day6_input.txt"
+def main(file):
+    with open(file) as f:
+        l = [l.strip() for l in f.readlines()]
+        solve(l)
+
 
 if __name__ == "__main__":
-    with open(INPUT) as f:
-       l = f.readlines()
-    solve(l,2)
+    # main("aoc_day6_test.txt")
+    main("aoc_day6_input.txt")
