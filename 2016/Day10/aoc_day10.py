fc04820ed2d7700b1aef7ff5fed80c1b2ed44214
Petr Pražák <prazakp@gmail.com>
2020-12-11 19:58:55 +0100

    Advent of Code 2016 day 10
    add readme,
    refactor

--- 2016/Day10/aoc_day10.py
+++ 2016/Day10/aoc_day10.py
@@ -1,15 +1,7 @@
-# replace 10 with day
-
-"""
-
-http://adventofcode.com/2016/day/10
-
-
-"""
-
+# http://adventofcode.com/2016/day/10
 from __future__ import print_function
 from collections import defaultdict
-from functools import reduce
+from math import prod
 
 
 def set_bot_data(bots, outputs, bot, value):
@@ -22,7 +14,6 @@ def set_bot_data(bots, outputs, bot, value):
 
 
 def bot_run(bots, outputs, bot):
-
     prg = bots[bot]["p"]
     if prg:
         l = bots[bot]["d"]
@@ -41,39 +32,35 @@ def bot_run(bots, outputs, bot):
 def solve(lines):
     bots = defaultdict(dict)
     outputs = {}
-    for line in lines:
-        line = line.strip()
-        part = line.split()
+    for part in lines:
         # value 11 goes to bot 124
         if part[0] == "value":
             value = int(part[1])
             bot = int(part[5])
             set_bot_data(bots, outputs, bot, value)
-
         # bot 153 gives low to bot 105 and high to bot 10
         # bot 12 gives low to output 4 and high to bot 125
         elif part[0] == "bot":
             bot = int(part[1])
             low_to = int(part[6])
             high_to = int(part[11])
-            data = []
-            if bot in bots:
-                data = bots[bot]["d"]
+            data = bots[bot]["d"] if bot in bots else []
             bots[bot] = {"p": [(part[5], low_to), (part[10], high_to)], "d": data}
             if len(data) == 2:
                 bot_run(bots, outputs, bot)
-
         else:
             raise NotImplementedError(part)
 
-    print([k for k in bots if 61 in bots[k]["d"] and 17 in bots[k]["d"]][0])
-    print(reduce(lambda x, y: x * y, [outputs[0], outputs[1], outputs[2]]))
+    print("Part 1:", [k for k in bots
+                      if 61 in bots[k] ["d"] and 17 in bots[k]["d"]][0])
+    print("Part 2:", prod([outputs[0], outputs[1], outputs[2]]))
 
 
-INPUT = "aoc_day10_input.txt"
+def main(file):
+    with open(file) as f:
+        lines = [line.strip().split() for line in f.readlines()]
+        solve(lines)
+
 
 if __name__ == "__main__":
-    with open(INPUT) as f:
-        # read by lines
-        lines = f.readlines()
-        solve(lines)
+    main("aoc_day10_input.txt")
