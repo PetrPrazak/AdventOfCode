202bc0cdaa8d96e767e0387af6a764aecfa53030
Petr Pražák <prazakp@gmail.com>
2020-12-12 00:26:43 +0100

    Advent of Code 2015 day 1
    remove useless list

--- 2015/Day01/aoc2015_01.py
+++ 2015/Day01/aoc2015_01.py
@@ -1,6 +1,4 @@
-"""
-http://adventofcode.com/2015/day/1
-"""
+# http://adventofcode.com/2015/day/1
 from __future__ import print_function
 from collections import Counter
 
@@ -8,13 +6,13 @@ with open("input.txt") as f:
     data = f.read()
 
     # part 1
-    c = Counter(list(data))
+    c = Counter(data)
     print(c['('] - c[')'])
 
     # part 2
     floor = 0
     pos = 0
-    for p in list(data):
+    for p in data:
         pos += 1
         if p == '(':
             floor += 1
