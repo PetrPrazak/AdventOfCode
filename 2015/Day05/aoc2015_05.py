f6d12aa4aaf567801cbf41293505a60647a5af8c
Petr Pražák <prazakp@gmail.com>
2020-12-12 18:10:57 +0100

    Advent of Code 2015 day 5
    tweaks

--- 2015/Day05/aoc2015_05.py
+++ 2015/Day05/aoc2015_05.py
@@ -1,54 +1,45 @@
-"""
-http://adventofcode.com/2015/day/5
-"""
+# http://adventofcode.com/2015/day/5
 from __future__ import print_function
 from collections import Counter
 
 
 def is_nice(word):
     for f in ["ab", "cd", "pq", "xy"]:
-        idx = word.find(f)
-        if idx != -1:
+        if f in word:
             return False
 
     c = Counter(word)
-    l = [c[v] for v in list("aeiou")]
-    if sum(l) < 3:
+    if sum(c[v] for v in "aeiou") < 3:
         return False
 
     p = word[0]
     for i in range(1, len(word)):
         if p == word[i]:
-            break
+            return True
         p = word[i]
-    else:
-        i = len(word)
-
-    return i < len(word)
+    return False
 
 
-def is_nice2(word):
+def has_pair(word):
     p = word[0:2]
     for i in range(1, len(word)):
         idx = word[i + 1:].find(p)
         if idx != -1:
-            break
+            return True
         p = word[i:i + 2]
-    else:
-        i = len(word)
-    c = i < len(word)
-    if not c:
+    return False
+
+
+def is_nice2(word):
+    if not has_pair(word):
         return False
 
     p = word[0]
     for i in range(1, len(word) - 1):
         if p == word[i + 1]:
-            break
+            return True
         p = word[i]
-    else:
-        i = len(word) - 1
-
-    return i < len(word) - 1
+    return False
 
 
 def main():
