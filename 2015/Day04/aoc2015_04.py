23a4575e9c9dff83521566b5d5cca6cf389fb28b
Petr Pražák <prazakp@gmail.com>
2020-12-12 17:55:48 +0100

    Advent of Code 2015 day 4
    tweaks

--- 2015/Day04/aoc2015_04.py
+++ 2015/Day04/aoc2015_04.py
@@ -1,25 +1,17 @@
-"""
-http://adventofcode.com/2015/day/4
-"""
+# http://adventofcode.com/2015/day/4
 from __future__ import print_function
 from hashlib import md5
 
 inputdata = "iwrupvqb"
 
-try:
-    xrange
-except NameError:
-    xrange = range
-
 
 def find_first_md5_zeros(zeros):
     prefix = "0" * zeros
-    for i in xrange(100000000):
+    for i in range(100000000):
         data = inputdata + str(i)
         s = md5(data.encode('ascii')).hexdigest()
         if s[:zeros] == prefix:
             return i
-
     return 0
 
 
