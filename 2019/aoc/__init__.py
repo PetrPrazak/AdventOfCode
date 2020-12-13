5b868bb7695ffae21f1f2c2afe584324a71b80cb
Petr Prazak <prazakp@gmail.com>
2020-12-13 18:58:20 +0100

    Advent of Code 2019
    add find_shortest_path

--- 2019/aoc/__init__.py
+++ 2019/aoc/__init__.py
@@ -117,6 +117,19 @@ def Path(previous, s):
     return [] if (s is None) else Path(previous, previous[s]) + [s]
 
 
+# Code by Eryk Kopczyński
+def find_shortest_path(graph, start, end):
+    dist = {start: [start]}
+    q = deque(start)
+    while len(q):
+        at = q.popleft()
+        for next in graph[at]:
+            if next not in dist:
+                dist[next] = [dist[at], next]
+                q.append(next)
+    return dist.get(end)
+
+
 def minmax_tuples(tuple_list, element=0):
     res = sorted(tuple_list, key=lambda k: k[element])
     return res[0][element], res[-1][element]
