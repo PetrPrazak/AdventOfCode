e0f2699c4296f7da019f03dd54fdf25e788835c2
Petr Pražák <prazakp@gmail.com>
2020-12-13 12:53:00 +0100

    Advent of Code 2020 day 13

---
+++ 2016/template/README.md
@@ -0,0 +1,5 @@
+# x
+Copyright (c) Eric Wastl
+#### [Source](https://adventofcode.com/2016/day/xx)
+
+## Part 1
---
+++ 2016/template/aoc_day10.py
@@ -0,0 +1,31 @@
+# http://adventofcode.com/2016/day/XX
+from __future__ import print_function
+from collections import defaultdict, Counter
+from pprint import pprint
+
+
+def solve(lines):
+    # part 1
+    result = 0
+    print("Part 1:", result)
+    # part 2
+    result = 0
+    print("Part 2:", result)
+
+
+def parse(line):
+    return line
+
+
+def main(file):
+    with open(file) as f:
+        # read all in once
+        data = f.read()
+        solve(data)
+        # read by lines
+        lines = [parse(l.strip()) for l in f.readlines()]
+        solve(lines)
+
+
+if __name__ == "__main__":
+    main("input.txt")
