85911fc0daaab8294c1f50de3c14e5e3b95ae7b7
Petr Pražák <prazakp@gmail.com>
2020-12-16 22:06:48 +0100

    Advent of code 2017 day 13
    renaming, tweaks

--- 2017/13/aoc2017_13.py
+++ 2017/13/aoc2017_13.py
@@ -1,10 +1,4 @@
-
-"""
-
-http://adventofcode.com/2017/day/13
-
-
-"""
+# http://adventofcode.com/2017/day/13
 from __future__ import print_function
 
 
@@ -14,22 +8,21 @@ def solve(lines):
     layers = {int(pos): int(height) for pos, height in parts}
 
     # part 1
-    print(sum([n * layers[n] for n in layers if not n % ((layers[n] - 1) * 2)]))
+    print(sum([n * layers[n]
+               for n in layers if not n % ((layers[n] - 1) * 2)]))
 
     # part 2
     for d in range(0, 10000000, 2):
         try:
-            next(iter(n for n in layers if not (n + d) % ((layers[n] - 1) * 2)))
+            next(iter(n for n in layers if not (n + d) %
+                      ((layers[n] - 1) * 2)))
         except StopIteration:
             print(d)
             break
 
 
-INPUT = "aoc_day13_input.txt"
-# INPUT = "aoc_day13_test.txt"
-
 if __name__ == "__main__":
-    with open(INPUT) as f:
+    with open("input.txt") as f:
         # read by lines
         lines = f.readlines()
         solve(lines)
