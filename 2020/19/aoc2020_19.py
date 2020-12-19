bc2f312d72d603f4d15d9f37d47bfdde797adaf0
Petr Pražák <prazakp@gmail.com>
2020-12-19 13:33:49 +0100

    Advent of Code 2020 day 19

---
+++ 2020/19/aoc2020_19.py
@@ -0,0 +1,73 @@
+# https://adventofcode.com/2020/day/19
+from __future__ import print_function
+import re
+
+
+def reduce_rules(rules, term="0"):
+    right = rules[term]
+    if type(right) is list:
+        out = []
+        for part in right:
+            right_rules = (reduce_rules(rules, t) for t in part)
+            out.append(''.join(right_rules))
+        if len(out) > 1:
+            return '(?:' + '|'.join(out) + ')'
+        else:
+            return out[0]
+    else:
+        return right
+
+
+def part1(data):
+    rules, data = data
+    reg = reduce_rules(rules)
+    return sum(bool(re.fullmatch(reg, line)) for line in data)
+
+
+def part2(data):
+    # part 2
+    rules, data = data
+    rules["8"] = [['42', 'P']]
+    rules["P"] = "+"
+    # 11: 42{1}31{1} | ... | 42{x}31{x}
+    rule11 = []
+    for i in range(1, 7):
+        rule11.append(['42', f'R{i}', '31', f'R{i}'])
+        rules[f"R{i}"] = f"{{{i}}}"
+    rules["11"] = rule11
+    reg = "^" + reduce_rules(rules) + "$"   # FCK!
+    return sum(bool(re.match(reg, line)) for line in data)
+
+
+def process(data):
+    # part 1
+    print("part 1:", part1(data))
+    print("part 2:", part2(data))
+
+
+def parse_rules(rules):
+    rules_dict = dict()
+    for rule in rules.split("\n"):
+        start, end = rule.split(": ")
+        if end[0] == '"':
+            rules_dict[start] = end.strip('"')
+        else:
+            target = [list(part.split(" ")) for part in end.split(" | ")]
+            rules_dict[start] = target
+    return rules_dict
+
+
+def load_data(fileobj):
+    rules, data = fileobj.read().split("\n\n")
+    return parse_rules(rules), [l for l in data.split()]
+
+
+def main(file):
+    print(file)
+    with open(file) as f:
+        process(load_data(f))
+
+
+if __name__ == "__main__":
+    main("test.txt")
+    main("input.txt")
