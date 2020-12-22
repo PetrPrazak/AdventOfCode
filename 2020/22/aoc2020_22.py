fafb6092bc7b0a41a9ae9d940860b2b30fa4a300
Petr Pražák <prazakp@gmail.com>
2020-12-22 18:02:30 +0100

    Advent of Code 2020 day 22
    small refactorings

--- 2020/22/aoc2020_22.py
+++ 2020/22/aoc2020_22.py
@@ -4,6 +4,7 @@ from collections import deque
 from copy import deepcopy
 from itertools import islice
 
+
 def add_cards(deck, winning, losing):
     deck.append(winning)
     deck.append(losing)
@@ -16,7 +17,7 @@ def play(deck1, deck2):
             add_cards(deck1, card1, card2)
         else:
             add_cards(deck2, card2, card1)
-    return deck1 if deck1 else deck2
+    return deck1 or deck2
 
 
 def copy_deck(deck, length):
@@ -33,20 +34,18 @@ def rec_play(deck1, deck2, level=0):
 
         card1, card2 = deck1.popleft(), deck2.popleft()
         if card1 <= len(deck1) and card2 <= len(deck2):
-            if rec_play(copy_deck(deck1, card1), copy_deck(deck2, card2), level+1):
-                add_cards(deck1, card1, card2)
-            else:
-                add_cards(deck2, card2, card1)
+            is_winner1 = rec_play(copy_deck(deck1, card1), copy_deck(deck2, card2), level+1)
         else:
-            if card1 > card2:
-                add_cards(deck1, card1, card2)
-            else:
-                add_cards(deck2, card2, card1)
+            is_winner1 = card1 > card2
+        if is_winner1:
+            add_cards(deck1, card1, card2)
+        else:
+            add_cards(deck2, card2, card1)
 
     if level:
         return bool(deck1)
     else:
-        return deck1 if deck1 else deck2
+        return deck1 or deck2
 
 
 def sum_deck(deck):
