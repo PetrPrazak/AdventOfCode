77d27b4d948b81d93d64b316fbc2ea1e2f00b11e
Petr Pražák <prazakp@gmail.com>
2021-12-07 12:12:01 +0100

    Advent of Code 2021 day 7
    optimizing

--- 2021/07/aoc2021_07.py
+++ 2021/07/aoc2021_07.py
@@ -1,20 +1,18 @@
 # https://adventofcode.com/2021/day/7
 from __future__ import print_function
 from pathlib import Path
-from functools import cache
+from math import inf
 
 
-@cache
 def sum_up_to(n):
-    return sum(range(1, n+1))
+    return n * (n + 1) // 2
 
 
 def find_best_position(crabs, valuate_distance):
-    min_fuel = None
+    min_fuel = inf
     for r in range(min(crabs), max(crabs) + 1):
         fuel = sum(valuate_distance(c - r) for c in crabs)
-        if min_fuel is None or fuel < min_fuel:
-            min_fuel = fuel
+        min_fuel = min(min_fuel, fuel)
     return min_fuel
 
 
