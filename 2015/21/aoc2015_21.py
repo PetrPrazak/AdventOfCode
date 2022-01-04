18f6f31fa4fdbbf1dd59a74e5424f215709aac25
Petr Pražák <prazakp@gmail.com>
2022-01-04 20:06:22 +0100

    Advent of Code 2015 day 21

---
+++ 2015/21/aoc2015_21.py
@@ -0,0 +1,85 @@
+# https://adventofcode.com/2015/day/21
+from __future__ import print_function
+from pathlib import Path
+from collections import namedtuple
+from itertools import product
+
+
+START_HIT_POINTS = 100
+
+Item = namedtuple('Item', 'name cost damage armor')
+Weapons = [
+    Item("Dagger", 8, 4, 0),
+    Item("Shortsword", 10, 5, 0),
+    Item("Warhammer", 25, 6, 0),
+    Item("Longsword", 40, 7, 0),
+    Item("Greataxe", 74, 8, 0)
+]
+
+Armor = [
+    Item(None, 0, 0, 0),
+    Item("Leather", 13, 0, 1),
+    Item("Chainmail", 31, 0, 2),
+    Item("Splintmail", 53, 0, 3),
+    Item("Bandedmail", 75, 0, 4),
+    Item("Platemail", 102, 0, 5)
+]
+
+Rings = [
+    Item(None, 0, 0, 0),
+    Item("Damage +1", 25, 1, 0),
+    Item("Damage +2", 50, 2, 0),
+    Item("Damage +3", 100, 3, 0),
+    Item("Defense +1", 20, 0, 1),
+    Item("Defense +2", 40, 0, 2),
+    Item("Defense +3", 80, 0, 3)
+]
+
+
+def fight(player, boss):
+    """ returns True if player wins """
+    pl_hp, boss_hp = player[0], boss[0]
+    pl_damage = max(1, player[1] - boss[2])
+    boss_damage = max(1, boss[1] - player[2])
+    p1, p2 = divmod(pl_hp, boss_damage)
+    b1, b2 = divmod(boss_hp, pl_damage)
+    return p1 > b1 or p1 == b1 and p2 >= b2
+
+
+def process(data):
+    min_cost = 9999
+    max_cost = 0
+    for w, a, r1, r2 in product(Weapons, Armor, Rings, Rings):
+        if r1.name and r2.name and r1 == r2:
+            continue
+        cost = w.cost + a.cost + r1.cost + r2.cost
+        damage = w.damage + a.damage + r1.damage + r2.damage
+        armor = w.armor + a.armor + r1.armor + r2.armor
+        if fight([START_HIT_POINTS, damage, armor], data):
+            min_cost = min(cost, min_cost)
+        else:
+            max_cost = max(cost, max_cost)
+
+    # part 1
+    print("part 1:", min_cost)
+    # part 2
+    print("part 2:", max_cost)
+
+
+def parse_line(line):
+    return int(line.strip().split(': ')[1])
+
+
+def load_data(fileobj):
+    return [parse_line(line) for line in fileobj.readlines()]
+
+
+def main(file="input.txt"):
+    print(file)
+    with Path(__file__).parent.joinpath(file).open() as f:
+        process(load_data(f))
+
+
+if __name__ == "__main__":
+    # main("test.txt")
+    main()
