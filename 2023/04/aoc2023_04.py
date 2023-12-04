e4927de6ce13d8a22d08edaaba67cb0708ab42e5
Petr Pražák <prazakp@gmail.com>
2023-12-04 11:07:31 +0100

    Advent of Code 2023 day 4

---
+++ 2023/04/aoc2023_04.py
@@ -0,0 +1,46 @@
+# https://adventofcode.com/2023/day/4
+from pprint import pprint
+from pathlib import Path
+from collections import defaultdict
+import time
+
+
+def process(data):
+    p1total = 0
+    copies = defaultdict(int)
+    for card, (win, have) in enumerate(data):
+        same_numbers = len(set(win).intersection(set(have)))
+        # part 1
+        p1total += 2 ** (same_numbers - 1) if same_numbers else 0
+        # part 2
+        copies[card] += 1
+        if same_numbers:
+            for won_card in range(card + 1, min(len(data), card + same_numbers + 1)):
+                copies[won_card] += copies[card]
+    result = sum(copies.values())
+    print("part 1:", p1total)
+    print("part 2:", result)
+
+
+def parse_line(line):
+    _, nums = line.split(": ")
+    win, have = nums.split(' | ')
+    return list(map(int, win.split())), list(map(int, have.split()))
+
+
+def load_data(fileobj):
+    return [parse_line(line.rstrip()) for line in fileobj.readlines()]
+
+
+def main(file="input.txt"):
+    print(file)
+    with Path(__file__).parent.joinpath(file).open() as f:
+        t1 = time.perf_counter()
+        process(load_data(f))
+        t2 = time.perf_counter()
+        print(f"Finished in {t2 - t1:.3f} s")
+
+
+if __name__ == "__main__":
+    # main("test.txt")
+    main()
