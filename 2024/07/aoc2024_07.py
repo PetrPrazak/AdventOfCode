02dfd02f8c92071d05733fd65cd6e577ca617b62
Petr Pražák <prazakp@gmail.com>
2024-12-07 10:49:06 +0100

    Advent of Code 2024 day 7

---
+++ 2024/07/aoc2024_07.py
@@ -0,0 +1,55 @@
+# https://adventofcode.com/2024/day/7
+from pathlib import Path
+import time
+
+
+def try_operators(res, nums, part=1):
+    stack = [nums[0]]
+    for n in nums[1:]:
+        newstack = []
+        for r in stack:
+            rplus = r + n
+            if rplus <= res:
+                newstack.append(rplus)
+            rmul = r * n
+            if rmul <= res:
+                newstack.append(rmul)
+            if part == 2:
+                rconcat = int(str(r)+str(n))
+                if rconcat <= res:
+                    newstack.append(rconcat)
+        stack = newstack
+    return res in stack
+
+
+def process(data):
+    # part 1
+    result = sum(res for res, nums in data if try_operators(res, nums))
+    print("part 1:", result)
+    # part 2
+    result = sum(res for res, nums in data if try_operators(res, nums, part=2))
+    print("part 2:", result)
+
+
+def parse_line(line):
+    result, nums = line.split(': ')
+    nums = list(map(int, nums.split()))
+    return int(result), nums
+
+
+def load_data(fileobj):
+    return [parse_line(line.rstrip()) for line in fileobj.readlines()]
+
+
+def main(file="input.txt"):
+    print(file)
+    with Path(__file__).parent.joinpath(file).open() as f:
+        t1 = time.perf_counter()
+        process(load_data(f))
+        t2 = time.perf_counter()
+        print(f"Finished in {t2 - t1:.3f} s")
+
+
+if __name__ == "__main__":
+    # main("test.txt")
+    main()
