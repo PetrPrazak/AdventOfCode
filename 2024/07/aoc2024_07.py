930aa94a06eb06b4524cd91c55751d45b2af6d6a
Petr Pražák <prazakp@gmail.com>
2024-12-07 19:17:44 +0100

    Advent of Code 2024 day 7
    use better solution

--- 2024/07/aoc2024_07.py
+++ 2024/07/aoc2024_07.py
@@ -3,8 +3,6 @@ from pathlib import Path
 import time
 from math import log10
 
-def join(a, b): return a * 10 ** int(log10(b) + 1) + b
-
 
 def try_operators(res, nums, part=1):
     stack = [nums[0]]
@@ -18,26 +16,37 @@ def try_operators(res, nums, part=1):
             if rmul <= res:
                 newstack.append(rmul)
             if part == 2:
-                rconcat = join(r, n)
+                rconcat = r * 10 ** int(log10(n) + 1) + n
                 if rconcat <= res:
                     newstack.append(rconcat)
         stack = newstack
     return res in stack
 
 
+# much faster recursive solution, process the list backwards
+def check_nums(res, nums, part=1):
+    if len(nums) == 1:
+        return res == nums[0]
+    last, rest = nums[-1], nums[:-1]
+    if res % last == 0 and check_nums(res // last, rest, part):
+        return True
+    if res > last and check_nums(res - last, rest, part):
+        return True
+    if part == 2:
+        s_res, s_last = str(res), str(last)
+        if len(s_res) > len(s_last) and s_res.endswith(s_last) \
+                and check_nums(int(s_res[:-len(s_last)]), rest, part):
+            return True
+    return False
+
+
 def process(data):
     # part 1
-    result = 0
-    rest = []
-    for res, nums in data:
-        if try_operators(res, nums):
-            result += res
-        else:
-            rest.append((res, nums))
+    result = sum(res for res, nums in data if check_nums(res, nums))
     print("part 1:", result)
 
     # part 2
-    result += sum(res for res, nums in rest if try_operators(res, nums, part=2))
+    result = sum(res for res, nums in data if check_nums(res, nums, part=2))
     print("part 2:", result)
 
 
